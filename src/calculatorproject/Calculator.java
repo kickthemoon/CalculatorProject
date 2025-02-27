dfdce18f93a0a4a6beb83a4136d92908de269405
kickthemoon <soonch67890@gmail.com>
2025-02-27 17:51:50 +0900

    사칙연산 추입력받기

--- src/calculatorproject/Calculator.java
+++ src/calculatorproject/Calculator.java
@@ -6,15 +6,26 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("계산기 입니다. 두 숫자를 입력 하세요");
+        // 양의 정수 입력받기
+        System.out.println("계산기 입니다. 두 숫자를 입력하세요");
 
-        System.out.print("첫번째 숫자 입니다. ");
+        System.out.print("첫번째 숫자입니다. ");
         int firstInterger = scanner.nextInt();
 
-        System.out.print("두번째 숫자 입니다. ");
+        System.out.print("두번째 숫자입니다. ");
         int secondInterger = scanner.nextInt();
 
-        System.out.println("당신이 입력한 두 숫자 입니다. 첫번째는 " + firstInterger + " 이고 두번째는 " + secondInterger + " 입니다");
+        System.out.println("당신이 입력한 두 숫자입니다. 첫번째는 " + firstInterger + " 이고 두번째는 " + secondInterger + " 입니다");
 
+        // 사칙연산 기호 입력받기
+        System.out.println("사칙연산 기호를 넣어주세요 (+, -, *, /): ");
+        String arithmeticOperations = scanner.nextLine();
+        char operation = arithmeticOperations.charAt(0);
+
+        if (operation == '+'||operation == '-'||operation == '*'||operation == '/') {
+            System.out.println("사칙연산 기호는 " + operation + "입니다");
+        } else {
+            System.out.println("사칙연산 기호가 아닙니다.");
+        }
     }
 }
