228df6da169744c59307ee51447ebc43b1653d46
kickthemoon <soonch67890@gmail.com>
2025-02-27 18:12:53 +0900

    After removing the line break characters remaining in the buffer, input the four basic arithmetic operations

--- src/calculatorproject/Calculator.java
+++ src/calculatorproject/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         // 양의 정수 입력받기
         System.out.println("계산기 입니다. 두 숫자를 입력하세요");
 
@@ -17,14 +18,18 @@ public class Calculator {
 
         System.out.println("당신이 입력한 두 숫자입니다. 첫번째는 " + firstInterger + " 이고 두번째는 " + secondInterger + " 입니다");
 
-        // 사칙연산 기호 입력받기
+        scanner.nextLine(); // 버퍼에 남은 개행문자 제거
+
+        // 사칙연산 기호 입력받기 + 입력 받을때 까지 대기 하기
         System.out.println("사칙연산 기호를 넣어주세요 (+, -, *, /): ");
         String arithmeticOperations = scanner.nextLine();
         char operation = arithmeticOperations.charAt(0);
 
-        if (operation == '+'||operation == '-'||operation == '*'||operation == '/') {
-            System.out.println("사칙연산 기호는 " + operation + "입니다");
-        } else {
+        while (true) {
+            if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
+                System.out.println("사칙연산 기호는 " + operation + "입니다");
+                break;
+            }
             System.out.println("사칙연산 기호가 아닙니다.");
         }
     }
