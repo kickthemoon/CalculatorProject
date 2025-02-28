c9dedd767c174522c60167fb67e5ab03ee587e68
kickthemoon <soonch67890@gmail.com>
2025-02-28 11:57:58 +0900

    캡슐화 (게터와 세터) 사용

--- src/calculatorproject/App.java
+++ src/calculatorproject/App.java
@@ -7,33 +7,41 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
         String end;
+        double result;
+        int firstIntergerApp;
+        int secondIntergerApp;
 
         do {
             // 양의 정수 입력받기
             System.out.println("계산기 입니다. 두 숫자를 입력하세요");
 
             System.out.print("첫번째 숫자입니다. ");
-            calculator.firstInterger = scanner.nextInt();
+            firstIntergerApp = scanner.nextInt();
+            calculator.setFirstInterger(firstIntergerApp);
 
             System.out.print("두번째 숫자입니다. ");
-            calculator.secondInterger = scanner.nextInt();
+            secondIntergerApp = scanner.nextInt();
+            calculator.setSecondInterger(secondIntergerApp);
 
-            System.out.println("당신이 입력한 두 숫자입니다. 첫번째는 " + calculator.firstInterger + " 이고 두번째는 " + calculator.secondInterger + " 입니다");
+            System.out.println("당신이 입력한 두 숫자입니다. 첫번째는 " + 1 + " 이고 두번째는 " + 2 + " 입니다");
 
             scanner.nextLine(); // 버퍼에 남은 개행문자 제거
 
             System.out.println("사칙연산 기호를 넣어주세요 (+, -, *, /): ");
             String arithmeticOperations = scanner.nextLine();
-            calculator.operation = arithmeticOperations.charAt(0);
+            calculator.setOperation(arithmeticOperations.charAt(0));
 
             // 사칙 연산 계산기
             calculator.Calculator();
 
+            // 결과 받아오기
+            result = calculator.getResult();
+
             //잘못 입력했을때 결과값 출력하지 않기
-            if(!calculator.noResult) {
-                System.out.println("값은: " + calculator.result);
+            if(!calculator.noshowResult) {
+                System.out.println("값은: " + result);
             }
-            calculator.noResult = false;
+            calculator.noshowResult = false;
 
             System.out.println("계산기를 종료하고 싶으면 exit를 입력하세요.");
             end = scanner.nextLine();
--- src/calculatorproject/Calculator.java
+++ src/calculatorproject/Calculator.java
@@ -1,11 +1,28 @@
 package calculatorproject;
 
 public class Calculator {
-    int firstInterger;
-    int secondInterger;
-    double result;
-    char operation;
-    boolean noResult;
+    private int firstInterger;
+    private int secondInterger;
+    private double result;
+    private char operation;
+    public boolean noshowResult;
+
+    //세터
+    void setFirstInterger(int firstInterger) {
+        this.firstInterger = firstInterger;
+    }
+    void setSecondInterger(int secondInterger) {
+        this.secondInterger = secondInterger;
+    }
+    void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    // 게터
+    double getResult(){
+        return this.result;
+    }
+
 
     public double Calculator() {
 
@@ -26,12 +43,12 @@ public class Calculator {
                         break;
                     } else {
                         System.out.println("두번째 숫자에는 0이 오면 나눌 수 없습니다.");
-                        noResult = true;
+                        noshowResult = true;
                     }
             }
         } else {
             System.out.println("사칙연산 기호가 아닙니다.");
-            noResult = true;
+            noshowResult = true;
         }
         return result;
     }
