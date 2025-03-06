9502302ee985b805869d4ca4afc448b4559496a1
kickthemoon <soonch67890@gmail.com>
2025-03-06 11:51:57 +0900

    메인이 들어 있는 App클래스를 Main클래스로 이름 바꿈

--- src/calculatorproject/Main.java
+++ src/calculatorproject/Main.java
@@ -2,7 +2,7 @@ package calculatorproject;
 
 import java.util.Scanner;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
